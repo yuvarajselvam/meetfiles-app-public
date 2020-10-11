08ca9f6403fabdba936312037b837cbb09381859
Yuvaraj Selvam <imyuvarajselvam@gmail.com>
2020-10-12 00:56:25 +0530

    Added Env file

---
+++ Backend/app/utils/response.py
@@ -0,0 +1,27 @@
+import os
+import jwt
+import datetime
+from flask import jsonify
+
+
+def make_response(response_tuple):
+    (message, code) = response_tuple
+    response = jsonify(message=message)
+    response.status_code = code
+    return response
+
+
+def generate_auth_token_response(userId):
+    exp = datetime.datetime.utcnow() + datetime.timedelta(days=30)
+    payload = {"userId": userId, "exp": exp}
+    auth_token = jwt.encode(payload, os.getenv("JWT_SECRET_KEY")).decode()
+    response = jsonify(authToken=auth_token, userId=userId, expires=exp)
+    response.status_code = 200
+    return response
+
+
+def check_required_fields(required_fields, request_json):
+    for k in required_fields:
+        if k not in request_json:
+            response = make_response((f"{k} field is mandatory.", 400))
+            return response
--- Backend/run.py
+++ Backend/run.py
@@ -2,10 +2,12 @@ import os
 import logging
 
 from flask import Flask
+from dotenv import load_dotenv
 
+load_dotenv('.env')
 app = Flask(__name__)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
